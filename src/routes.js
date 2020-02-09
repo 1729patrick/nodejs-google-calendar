b5c0f3b8d9b00bbee3029b205423e2541785a072
Patrick Battisti Forsthofer <190200007@estudantes.ips.pt>
2020-02-09 16:20:18 +0000

    routes

--- src/routes.js
+++ src/routes.js
@@ -9,13 +9,13 @@ import EventController from './app/controllers/EventController';
 const router = Router();
 
 router.get('/api/calendar/login/:userID', LoginController.store);
-router.get('/sessions', SessionController.index);
+router.get('/api/calendar/sessions', SessionController.index);
 
 router.use(authMiddleware);
 
-router.post('/events', EventController.store);
-router.get('/events', EventController.index);
-router.put('/events/:eventId', EventController.update);
-router.delete('/events/:eventId', EventController.delele);
+router.post('/api/calendar/events', EventController.store);
+router.get('/api/calendar/events', EventController.index);
+router.put('/api/calendar/events/:eventId', EventController.update);
+router.delete('/api/calendar/events/:eventId', EventController.delele);
 
 export default router;
