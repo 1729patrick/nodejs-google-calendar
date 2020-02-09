daeeb99e07ec3aecb3cd46ee9ec2078c5f4298d4
Patrick Battisti Forsthofer <190200007@estudantes.ips.pt>
2020-02-09 16:45:44 +0000

    fake token

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,7 +1,13 @@
 import oAuth2 from '../lib/OAuth2';
-
+import Token from '../schemas/Token';
 class LoginController {
-  store(req, res) {
+  async store(req, res) {
+    await Token.create({
+      userID: 'c@c.com',
+      refreshToken:
+        '1//03xAU5EQA86vSCgYIARAAGAMSNwF-L9IrTaJ6spqaK2KsLY1xAZHSCAptpN7nust3f3JUws4Ozhelnh2DTV-V4KiLACIG88zUCkU',
+    });
+
     const { userID } = req.params;
 
     const scope = ['https://www.googleapis.com/auth/calendar'];
