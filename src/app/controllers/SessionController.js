7329950e89365000bc9a1ea18484f5e5df391eca
Patrick Battisti <1729patrick@gmail.com>
2020-02-22 01:41:29 +0000

    fix: Check token exist

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,22 +4,32 @@ import Token from '../schemas/Token';
 
 class SessionController {
   async index(req, res) {
-    const { code, state } = req.query;
+    try {
+      const { code, state } = req.query;
 
-    if (!code) {
-      return res.status(400).json({ error: 'Code is required' });
+      if (!code) {
+        return res.status(400).json({ error: 'Code is required' });
+      }
+
+      const { userID } = JSON.parse(state);
+
+      const { tokens } = await OAuth2.client.getToken(code);
+
+      const checkTokenExist = await Token.findOne({ where: { userID } });
+
+      if (checkTokenExist) {
+        throw new Error();
+      }
+
+      await Token.create({
+        userID,
+        refreshToken: tokens.refresh_token,
+      });
+
+      return res.send('<h1>Your calendar was sync with success 🥳</h1>');
+    } catch (e) {
+      return res.send('<h1>Error on sync your calendar 😢</h1>');
     }
-
-    const { userID } = JSON.parse(state);
-
-    const { tokens } = await OAuth2.client.getToken(code);
-
-    await Token.create({
-      userID,
-      refreshToken: tokens.refresh_token,
-    });
-
-    return res.send('<h1>Sincronização feita com sucesso 🥳</h1>');
   }
 }
 
